9e67b3f63f35f2d3cead92f72fc1b6391e2b26d0
DESKTOP-4ONK187\ACER NITRO 5 <hoanglong200703@gmail.com>
2025-12-13 18:35:34 +0700

    Update scroll-to-top button in ViewQuiz: Remove margin from the button icon for a cleaner appearance and improved alignment.

--- client/src/pages/ViewQuiz.tsx
+++ client/src/pages/ViewQuiz.tsx
@@ -613,7 +613,7 @@ export default function ViewQuiz(props: ViewQuizProps) {
             className="rounded-full shadow-lg hover:shadow-xl transition-all duration-200"
             title="Cuộn lên đầu trang"
           >
-            <ArrowUp className="w-5 h-5 mr-2" />
+            <ArrowUp className="w-5 h-5" />
           </Button>
         </div>
       </div>
